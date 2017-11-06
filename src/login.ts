2984f1f678886d84a2b978f1cf0417cad42464f4
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2017-11-06 15:32:46 +0100

    chore: msi login patchd

--- src/login.ts
+++ src/login.ts
@@ -1,13 +1,14 @@
 import * as msRest from "ms-rest";
 import * as msRestAzure from "ms-rest-azure";
+import * as request from "request";
 import * as winston from "winston";
 
 const subscriptionId = process.env.ARM_SUBSCRIPTION_ID as string;
 
-const msiPort =
-  typeof process.env.MSI_PORT === "string"
-    ? Number(process.env.MSI_PORT)
-    : 50342;
+const getMsiPort = (enpoint: string) => {
+  const url = new URL(enpoint);
+  return parseInt(url.port, 10);
+};
 
 process.on("unhandledRejection", e => winston.error(e));
 
@@ -18,7 +19,47 @@ export interface ICreds {
 
 export const loginWithMsi = async () => {
   const creds = await msRestAzure.loginWithMSI({
-    port: msiPort
+    port: getMsiPort(process.env.MSI_ENDPOINT as string)
   });
+  // tslint:disable-next-line:no-object-mutation
+  creds.getToken = getToken.bind(creds);
   return { creds, subscriptionId };
+};
+
+/**
+ * Patched getToken()
+ * see https://github.com/Azure/azure-sdk-for-node/issues/2292
+ */
+export const getToken = (
+  callback: (
+    error: Error | undefined,
+    result?: {
+      readonly token_type: string;
+      readonly access_token: string;
+    }
+  ) => void
+): void => {
+  const postUrl = process.env.MSI_ENDPOINT as string;
+  const reqOptions = this.prepareRequestOptions();
+  request.post(postUrl, reqOptions, (err, _, body) => {
+    if (err) {
+      return callback(err);
+    }
+    try {
+      const tokenResponse = JSON.parse(body);
+      if (!tokenResponse.token_type) {
+        throw new Error(
+          `Invalid token response, did not find token_type. Response body is: ${body}`
+        );
+      } else if (!tokenResponse.access_token) {
+        throw new Error(
+          `Invalid token response, did not find access_token. Response body is: ${body}`
+        );
+      }
+
+      return callback(undefined, tokenResponse);
+    } catch (error) {
+      return callback(error);
+    }
+  });
 };
