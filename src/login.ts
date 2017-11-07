a75984d9144d75b5bd6e49244b0dc934da55f788
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2017-11-07 09:31:15 +0100

    refactor: updatae ms-rest-azure package

--- src/login.ts
+++ src/login.ts
@@ -1,15 +1,8 @@
 import * as msRest from "ms-rest";
+import * as msRestAzure from "ms-rest-azure";
 import * as winston from "winston";
 
-/**
- * We cannot use loginWithMSI()
- * see https://github.com/Azure/azure-sdk-for-node/issues/2292
- */
-import { MSITokenCredentials } from "./msi_token_credentials";
-
 const subscriptionId = process.env.ARM_SUBSCRIPTION_ID as string;
-const endpoint = process.env.MSI_ENDPOINT as string;
-const secret = process.env.MSI_SECRET as string;
 
 process.on("unhandledRejection", e => winston.error(e));
 
@@ -18,17 +11,13 @@ export interface ICreds {
   readonly subscriptionId: string;
 }
 
-export const loginWithMsi = (): Promise<ICreds> => {
-  return new Promise((resolve, reject) => {
-    const creds = new MSITokenCredentials({
-      endpoint,
-      secret
-    });
-    creds.getToken(err => {
-      if (err) {
-        return reject(err);
-      }
-      return resolve({ creds, subscriptionId });
-    });
-  });
+/**
+ * The following call expects MSI_ENDPOINT and MSI_SECRET
+ * environment variables to be set. They don't appear
+ * in the App Service settings; you can check them
+ * using Kudu console.
+ */
+export const loginWithMsi = async () => {
+  const creds = await msRestAzure.loginWithAppServiceMSI();
+  return { subscriptionId, creds };
 };
