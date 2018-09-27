1e3f44927aa23f2dabe7d4738dec758359d65125
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2018-09-27 14:47:53 +0200

    fix service payload

--- build/middlewares/extract_payload.js
+++ build/middlewares/extract_payload.js
@@ -1,10 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const responses_1 = require("italia-ts-commons/lib/responses");
+const logger_1 = require("../logger");
 function ExtractFromPayloadMiddleware(type) {
     return request => new Promise(resolve => {
         const validation = type.decode(request.body);
         const result = validation.mapLeft(responses_1.ResponseErrorFromValidationErrors(type));
+        logger_1.logger.debug("ExtractFromPayloadMiddleware %s %s => %s", type.name, JSON.stringify(request.body), JSON.stringify(result.value));
         resolve(result);
     });
 }
--- src/middlewares/extract_payload.ts
+++ src/middlewares/extract_payload.ts
@@ -4,6 +4,7 @@
 import * as t from "io-ts";
 import { IRequestMiddleware } from "italia-ts-commons/lib/request_middleware";
 import { ResponseErrorFromValidationErrors } from "italia-ts-commons/lib/responses";
+import { logger } from "../logger";
 
 export function ExtractFromPayloadMiddleware<S, A>(
   type: t.Type<A, S>
@@ -13,6 +14,12 @@ export function ExtractFromPayloadMiddleware<S, A>(
       const validation = type.decode(request.body);
       const result = validation.mapLeft(
         ResponseErrorFromValidationErrors(type)
+      );
+      logger.debug(
+        "ExtractFromPayloadMiddleware %s %s => %s",
+        type.name,
+        JSON.stringify(request.body),
+        JSON.stringify(result.value)
       );
       resolve(result);
     });
