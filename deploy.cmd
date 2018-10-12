294a368b96d8bdfc97e11f625129062410d5189a
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2018-10-12 15:49:14 +0200

    packages

--- deploy.cmd
+++ deploy.cmd
@@ -73,9 +73,9 @@ IF DEFINED KUDU_SELECT_NODE_VERSION_CMD (
     SET NODE_EXE=node
   )
 
-  SET NPM_CMD="!NODE_EXE!" "!NPM_JS_PATH!" --scripts-prepend-node-path
+  SET NPM_CMD="!NODE_EXE!" "!NPM_JS_PATH!"
 ) ELSE (
-  SET NPM_CMD=npm --scripts-prepend-node-path
+  SET NPM_CMD=npm
   SET NODE_EXE=node
 )
 
