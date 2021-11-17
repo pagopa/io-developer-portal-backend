1c2c291b6afe6e778ece9cc725c2283a82de7fbd
Daniele Manni <dan.manni@gmail.com>
2021-11-17 11:08:41 +0100

    [#IC-47] Admin can change service category and custom_special_flow

--- src/utils/conversions.ts
+++ src/utils/conversions.ts
@@ -4,6 +4,9 @@ import { pick } from "italia-ts-commons/lib/types";
 import { Service } from "../../generated/api/Service";
 import { ServiceMetadata } from "../../generated/api/ServiceMetadata";
 import { ServiceScopeEnum } from "../../generated/api/ServiceScope";
+import { SpecialServiceMetadata } from "../../generated/api/SpecialServiceMetadata";
+import { StandardServiceCategoryEnum } from "../../generated/api/StandardServiceCategory";
+import { StandardServiceMetadata } from "../../generated/api/StandardServiceMetadata";
 import { VisibleServicePayload } from "../../generated/api/VisibleServicePayload";
 import { IExtendedUserContract, isAdminUser } from "../apim_operations";
 import { ServicePayload } from "../controllers/services";
@@ -28,6 +31,23 @@ export const getServicePayloadUpdater = (user: IExtendedUserContract) => (
   const serviceMetadata: ServiceMetadata = {
     ...payload.service_metadata,
     // Scope for visible services cannot be changed
+    category:
+      (SpecialServiceMetadata.is(payload.service_metadata) ||
+        StandardServiceMetadata.is(payload.service_metadata)) &&
+      isAdminUser(user)
+        ? payload.service_metadata.category
+        : (SpecialServiceMetadata.is(originalService.service_metadata) ||
+            StandardServiceMetadata.is(originalService.service_metadata)) &&
+          !isAdminUser(user)
+        ? originalService.service_metadata.category
+        : StandardServiceCategoryEnum.STANDARD,
+    custom_special_flow:
+      SpecialServiceMetadata.is(payload.service_metadata) && isAdminUser(user)
+        ? payload.service_metadata.custom_special_flow
+        : SpecialServiceMetadata.is(originalService.service_metadata) &&
+          !isAdminUser(user)
+        ? originalService.service_metadata.custom_special_flow
+        : undefined,
     scope:
       !VisibleServicePayload.is(originalService) || isAdminUser(user)
         ? fromNullable(payload.service_metadata?.scope)
