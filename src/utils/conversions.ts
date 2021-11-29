14ced96f35d9515b72ace78e000fdcfaef110c39
Daniele Manni <BurnedMarshal@users.noreply.github.com>
2021-11-29 17:25:25 +0100

    [#IC-47] Admin can change service category and custom_special_flow (#142)
    * [#IC-47] Admin can change service category and custom_special_flow
    
    * [#IC-47] reduce code duplication with hasCategory util
    
    * [#IC-47] Add some comments
    
    * [#IC-47] Update link to admin spec from master branch

--- src/utils/conversions.ts
+++ src/utils/conversions.ts
@@ -4,9 +4,17 @@ import { pick } from "italia-ts-commons/lib/types";
 import { Service } from "../../generated/api/Service";
 import { ServiceMetadata } from "../../generated/api/ServiceMetadata";
 import { ServiceScopeEnum } from "../../generated/api/ServiceScope";
+import { SpecialServiceMetadata } from "../../generated/api/SpecialServiceMetadata";
+import { StandardServiceCategoryEnum } from "../../generated/api/StandardServiceCategory";
+import { StandardServiceMetadata } from "../../generated/api/StandardServiceMetadata";
 import { VisibleServicePayload } from "../../generated/api/VisibleServicePayload";
 import { IExtendedUserContract, isAdminUser } from "../apim_operations";
 import { ServicePayload } from "../controllers/services";
+
+export const hasCategory = (
+  s?: ServiceMetadata
+): s is SpecialServiceMetadata | StandardServiceMetadata =>
+  SpecialServiceMetadata.is(s) || StandardServiceMetadata.is(s);
 
 export const getServicePayloadUpdater = (user: IExtendedUserContract) => (
   originalService: Service,
@@ -27,6 +35,21 @@ export const getServicePayloadUpdater = (user: IExtendedUserContract) => (
 
   const serviceMetadata: ServiceMetadata = {
     ...payload.service_metadata,
+    category:
+      hasCategory(payload.service_metadata) && isAdminUser(user)
+        ? payload.service_metadata.category
+        : hasCategory(originalService.service_metadata) && !isAdminUser(user)
+        ? // Keep original category value if the user is not an admin
+          originalService.service_metadata.category
+        : StandardServiceCategoryEnum.STANDARD,
+    custom_special_flow:
+      SpecialServiceMetadata.is(payload.service_metadata) && isAdminUser(user)
+        ? payload.service_metadata.custom_special_flow
+        : SpecialServiceMetadata.is(originalService.service_metadata) &&
+          !isAdminUser(user)
+        ? // Keep original custom_special_flow value if the user is not an admin
+          originalService.service_metadata.custom_special_flow
+        : undefined,
     // Scope for visible services cannot be changed
     scope:
       !VisibleServicePayload.is(originalService) || isAdminUser(user)
