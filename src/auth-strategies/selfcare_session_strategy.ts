c709b27cea5b640d7ee0767a382761d1e6863efc
Emanuele De Cupis <ema.decu@gmail.com>
2021-12-16 15:25:34 +0100

    [#ic-139] add session token generation

--- src/auth-strategies/selfcare_session_strategy.ts
+++ src/auth-strategies/selfcare_session_strategy.ts
@@ -4,26 +4,32 @@
 import * as express from "express";
 import * as t from "io-ts";
 import * as passport from "passport";
+import { ulid } from "ulid";
 
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { OrganizationFiscalCode } from "@pagopa/ts-commons/lib/strings";
 import { ExtractJwt, Strategy as JwtStrategy } from "passport-jwt";
-import { processTokenInfo } from "./misc";
+import * as jwt from "jsonwebtoken";
+import { JWT, processTokenInfo } from "./misc";
 import { SelfCareIdentity } from "./selfcare_identity_strategy";
 
 /**
  *
  */
 export type SelfCareUser = t.TypeOf<typeof SelfCareUser>;
-export const SelfCareUser = t.interface({
-  family_name: t.string,
-  given_name: t.string,
-  oid: NonEmptyString,
-  organization: t.interface({
-    id: OrganizationFiscalCode,
-    role: NonEmptyString
+export const SelfCareUser = t.intersection([
+  JWT,
+  t.interface({
+    family_name: t.string,
+    given_name: t.string,
+    oid: NonEmptyString,
+    organization: t.interface({
+      fiscal_number: OrganizationFiscalCode,
+      id: NonEmptyString,
+      role: NonEmptyString
+    })
   })
-});
+]);
 
 /**
  * Calls a callback on the logged in user's profile.
@@ -60,7 +66,24 @@ export const setupSelfCareSessionStrategy = (
 
 export const createSessionToken = (
   data: SelfCareIdentity,
-  options: { signatureKey: string }
+  options: { audience: string; issuer: string; signatureKey: string }
 ): string => {
-  throw new Error("not implemented yet");
+  const payload = {
+    aud: options.audience,
+    exp: Math.floor(data.desired_exp.getTime() / 1000),
+    iat: Math.floor(Date.now() / 1000),
+    iss: options.issuer,
+    jti: ulid(),
+    sub: data.sub,
+    family_name: data.family_name,
+    given_name: data.name,
+    oid: data.fiscal_number,
+    organization: {
+      id: data.organization.id,
+      role: data.organization.role,
+      fiscal_number: data.organization.fiscal_number
+    }
+  };
+
+  return jwt.sign(payload, options.signatureKey, { algorithm: "RS256" });
 };
