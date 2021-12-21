2c41c85fd2ac6b19fe517af489f868e05375ea5f
Emanuele De Cupis <ema.decu@gmail.com>
2021-12-21 09:48:34 +0100

    wip

--- src/auth-strategies/selfcare_identity_strategy.ts
+++ src/auth-strategies/selfcare_identity_strategy.ts
@@ -36,18 +36,10 @@ export const SelfCareIdentity = t.intersection([
 export const setupSelfCareIdentityStrategy = (
   passportInstance: typeof passport,
   // tslint:disable-next-line:no-any
-  { audience, issuer }: any
+  { audience, issuer, secret }: any
 ) => {
   const STRATEGY_NAME = "selfcare-identity";
-  const secret = `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvPV/txTclqFrzGKRa5Rd
-wGR5h+gHQKNI2MOhr3B+DHJ8dUXL67MM56OkunZzW65Ir5k49E6TU/5AukCQpQms
-Fq/QziwGSebnm5g4x4jhXbcLTNjJW1pSLULGt01dlGbotkhSICbK3Zk3/dpZvnpQ
-pSqsGm91jKOaU5fRAL5Xj5Ke7/oLsMDGRRlCEUz3ROgQ+51RAYdcHuVLnsGJlFAE
-5RXRE3uMpk8arP9R5iHb4tzqKthDBht4gRBO947Ea3FJYACY39X5dOISFRAUnGdg
-FSkVgDV0NRBm+Z5/LQsGPt7DKCqHbjI2KbBoWZgJyv+a8mCF/EHHLyXa8m6+I1yo
-gwIDAQAB
------END PUBLIC KEY-----`;
+
   const strategy = new JwtStrategy(
     {
       algorithms: ["RS256"],
