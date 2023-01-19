1e419da0695ec0da5ebfae4ed4f5f9551beaee80
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2023-01-19 12:11:10 +0100

    [#IOPAE-126] Exclude MANAGE subscription from subscription list

--- src/utils/api_key.ts
+++ src/utils/api_key.ts
@@ -1,5 +1,22 @@
 /**
  * Utilities to handle subscriptions api key
  */
+export enum ApiKeyTypeEnum {
+  "MANAGE" = "MANAGE"
+}
 
 export const MANAGE_APIKEY_PREFIX = "MANAGE-";
+const MANAGE_APIKEY_FILTER = `startswith(name, '${MANAGE_APIKEY_PREFIX}')`;
+
+/**
+ * User Subscription list filter by api key type
+ *
+ * @param keyType
+ * @returns
+ */
+export const getSubscriptionFilterByApiKeyType = (
+  keyType?: ApiKeyTypeEnum
+): string =>
+  keyType === ApiKeyTypeEnum.MANAGE
+    ? `startswith(name, '${MANAGE_APIKEY_PREFIX}')`
+    : `not(${MANAGE_APIKEY_FILTER})`;
