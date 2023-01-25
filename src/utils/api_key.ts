a8474b3e2e8ce2fb83c1009474974475fe8f5d42
Giuseppe Di Pinto <118166285+giuseppedipinto@users.noreply.github.com>
2023-01-25 17:10:59 +0100

    [#IOPAE-122] Fetch and Add Manage Key (#197)

---
+++ src/utils/api_key.ts
@@ -0,0 +1,24 @@
+/**
+ * Utilities to handle subscriptions api key
+ */
+export enum ApiKeyTypeEnum {
+  "MANAGE" = "MANAGE"
+}
+
+export const MANAGE_APIKEY_PREFIX = "MANAGE-";
+const MANAGE_APIKEY_FILTER = `startswith(name, '${MANAGE_APIKEY_PREFIX}')`;
+
+/**
+ * User Subscription list filter by api key type
+ *
+ * _(if **keyType** is null, MANAGE Subscriptions are excluded)_
+ *
+ * @param keyType
+ * @returns
+ */
+export const getSubscriptionFilterByApiKeyType = (
+  keyType?: ApiKeyTypeEnum
+): string =>
+  keyType === ApiKeyTypeEnum.MANAGE
+    ? MANAGE_APIKEY_FILTER
+    : `not(${MANAGE_APIKEY_FILTER})`;
