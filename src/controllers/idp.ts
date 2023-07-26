15e80777d747cfc2b5b49d5d362dbc1a7a5bb729
Giuseppe Di Pinto <118166285+giuseppedipinto@users.noreply.github.com>
2023-07-26 09:22:01 +0200

    [#IOPAE-459] Lock new Selfcare Organization to access to Developer Portal (#207)

--- src/controllers/idp.ts
+++ src/controllers/idp.ts
@@ -2,6 +2,7 @@ import { readableReport } from "@pagopa/ts-commons/lib/reporters";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { ValidUrl } from "@pagopa/ts-commons/lib/url";
 import ApiManagementClient from "azure-arm-apimanagement";
+import { isNone } from "fp-ts/lib/Option";
 import * as t from "io-ts";
 import {
   IResponsePermanentRedirect,
@@ -10,11 +11,13 @@ import {
 import { UrlFromString } from "italia-ts-commons/lib/url";
 import {
   apimUserForSelfCareOrganization,
-  createApimUserIfNotExists
+  createApimUserIfNotExists,
+  formatApimAccountEmailForSelfcareOrganization,
+  getApimUser
 } from "../apim_operations";
 import { SelfCareIdentity } from "../auth-strategies/selfcare_identity_strategy";
 import { createSessionToken } from "../auth-strategies/selfcare_session_strategy";
-import { selfcareSessionCreds } from "../config";
+import { lockSelfcareCreateNewApimUser, selfcareSessionCreds } from "../config";
 import { logger } from "../logger";
 
 const withToken = (url: ValidUrl, idToken: string): ValidUrl => {
@@ -43,12 +46,26 @@ export async function resolveSelfCareIdentity(
     });
 
   try {
-    // ensure an apim account is created for every session,
-    // so that every user operation can rely on that
-    await createApimUserIfNotExists(
-      apimClient,
-      apimUserForSelfCareOrganization(selfcareIdentity.organization)
-    );
+    // Ensure an APIM account exists for every session,
+    // so that every user operation can rely on that.
+    // If account creation is allowed for the first usage, the account is either fetcher or created
+    // In such a case, the login fails when account creation fails.
+    // Otherwise the account is fetched and the login fails when the account is not found
+    const maybeApimUser = lockSelfcareCreateNewApimUser
+      ? await getApimUser(
+          apimClient,
+          formatApimAccountEmailForSelfcareOrganization(
+            selfcareIdentity.organization
+          )
+        )
+      : await createApimUserIfNotExists(
+          apimClient,
+          apimUserForSelfCareOrganization(selfcareIdentity.organization)
+        );
+    // block access if Apim user is not returned
+    if (isNone(maybeApimUser)) {
+      return ResponsePermanentRedirect(options.failureLoginPage);
+    }
 
     const token = createSessionToken(selfcareIdentity, {
       audience: options.audience,
