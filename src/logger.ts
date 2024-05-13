bae215ddd75b3f4231c3f69432042e9a8d6876f5
Gianmaria Scorza <gianmaria@scorza.dev>
2024-05-13 15:58:06 +0200

    fix unit test

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,7 @@
 /**
  * Winston logger shared configuration.
  */
-import * as appinsights from "applicationinsights";
+import * as appInsights from "applicationinsights";
 import * as logform from "logform";
 import { createLogger, format, transports } from "winston";
 import * as config from "./config";
@@ -15,42 +15,45 @@ export const logger = createLogger({
     format.simple(),
     printf(nfo => {
       const message = `${nfo.timestamp} [${nfo.level}]: ${nfo.message}`;
+
       // Invia il log a Application Insights
-      appinsights.defaultClient.trackTrace({
-        message,
-        severity: getSeverityLevel(nfo.level)
-      });
+      if (appInsights.defaultClient) {
+        appInsights.defaultClient.trackTrace({
+          message,
+          severity: getSeverityLevel(nfo.level)
+        });
+      }
       return message;
     })
   ),
   transports: [new transports.Console({ level: config.logLevel || "info" })]
 });
 
-function getSeverityLevel(level: string): appinsights.Contracts.SeverityLevel {
+function getSeverityLevel(level: string): appInsights.Contracts.SeverityLevel {
   switch (level) {
     case "error":
-      return appinsights.Contracts.SeverityLevel.Error;
+      return appInsights.Contracts.SeverityLevel.Error;
     case "warn":
-      return appinsights.Contracts.SeverityLevel.Warning;
+      return appInsights.Contracts.SeverityLevel.Warning;
     case "info":
-      return appinsights.Contracts.SeverityLevel.Information;
+      return appInsights.Contracts.SeverityLevel.Information;
     case "verbose":
     case "debug":
-      return appinsights.Contracts.SeverityLevel.Verbose;
+      return appInsights.Contracts.SeverityLevel.Verbose;
     default:
-      return appinsights.Contracts.SeverityLevel.Information;
+      return appInsights.Contracts.SeverityLevel.Information;
   }
 }
 
 export const startAppInsights = () => {
   // collect monotoring metrics automatically
-  appinsights
+  appInsights
     .setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
     .setAutoCollectConsole(false) // Disabilita la raccolta automatica dei log
-    .setDistributedTracingMode(appinsights.DistributedTracingModes.AI_AND_W3C);
+    .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C);
 
   // tslint:disable-next-line: no-object-mutation
-  appinsights.defaultClient.config.samplingPercentage = 33;
+  appInsights.defaultClient.config.samplingPercentage = 33;
 
-  appinsights.start();
+  appInsights.start();
 };
