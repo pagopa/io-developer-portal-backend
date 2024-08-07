edd197d0b4e0915c9725e58df853a623ded8c24c
Mario Mupo <mario.mupo@pagopa.it>
2024-08-07 13:18:04 +0200

    feat: added branch protection on master

--- infra/repository/github_branch_rules.tf
+++ infra/repository/github_branch_rules.tf
@@ -3,28 +3,28 @@ resource "github_branch_default" "default_master" {
   branch     = "master"
 }
 
-# resource "github_branch_protection" "protection_master" {
-#   repository_id = github_repository.this.name
-#   pattern       = "master"
+resource "github_branch_protection" "protection_master" {
+  repository_id = github_repository.this.name
+  pattern       = "master"
 
-#   required_status_checks {
-#     strict   = false
-#     contexts = []
-#   }
+  required_status_checks {
+    strict   = false
+    contexts = []
+  }
 
-#   require_conversation_resolution = true
+  require_conversation_resolution = true
 
-#   #tfsec:ignore:github-branch_protections-require_signed_commits
-#   require_signed_commits = false
+  #tfsec:ignore:github-branch_protections-require_signed_commits
+  require_signed_commits = false
 
-#   required_pull_request_reviews {
-#     dismiss_stale_reviews           = false
-#     require_code_owner_reviews      = true
-#     required_approving_review_count = 1
-#   }
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = false
+    require_code_owner_reviews      = true
+    required_approving_review_count = 1
+  }
 
-#   allows_deletions = false
-# }
+  allows_deletions = false
+}
 
 resource "github_branch_protection" "protection_gh_pages" {
   repository_id = github_repository.this.name
