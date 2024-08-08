8f59705e9613fd63348cc2f27bac113f056d2102
Mario Mupo <43968294+mamu0@users.noreply.github.com>
2024-08-08 11:22:08 +0200

    [EC-386] Azure DevOps pipelines migrated into GitHub Actions (#238)

---
+++ infra/identity/prod/locals.tf
@@ -0,0 +1,19 @@
+locals {
+  prefix    = "io"
+  env_short = "p"
+  env       = "prod"
+  location  = "italynorth"
+  project   = "${local.prefix}-${local.env_short}"
+  domain    = "devportal-backend"
+
+  repo_name = "io-developer-portal-backend"
+
+  tags = {
+    CostCenter     = "TS310 - PAGAMENTI & SERVIZI"
+    CreatedBy      = "Terraform"
+    Environment    = "Prod"
+    Owner          = "IO"
+    ManagementTeam = "IO Enti & Servizi"
+    Source         = "https://github.com/pagopa/io-developer-portal-backend/blob/master/infra/identity/prod"
+  }
+}
